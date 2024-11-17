72ebabb8690f9ea7bcaa5c2a5a48a2563410b03d
Vladislav <tekuzar@mail,ru>
2024-11-17 15:03:00 +0300

    Done!

--- src/Autorization.java
+++ src/Autorization.java
@@ -2,51 +2,32 @@ import exceptions.WrongLoginException;
 import exceptions.exceptions.WrongPasswordException;
 
 public class Autorization {
-    String login;
-    String password;
-    String confirmPassword;
 
-    public Autorization(String login, String password, String confirmPassword) {
-        this.login = login;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
-    }
-
-    public String getLogin(String login) {
-        return this.login;
-    }
-
-    public String getPassword(String password) {
-        return this.password;
-    }
-
-    public String getConfirmPassword(String confirmPassword) {
-        return this.confirmPassword;
-    }
 
     public static void check(String login, String password, String confirmPassword) {
+        checkLogin(login);
+        checkPassword(password, confirmPassword);
+    }
 
-        if (login.length() <= 20) {
-            if (login.matches("^[a-zA-Z0-9_]*$")) {
-                System.out.println("Логин удовлетворяет заданным требованиям");
-            } else {
-                throw new WrongLoginException();
-            }
+
+    public static void checkLogin(String login) {
+
+        if (login.length() <= 20 & login.matches("^[a-zA-Z0-9_]*$")) {
+            System.out.println("Логин удовлетворяет заданным требованиям");
         } else {
             throw new WrongLoginException();
         }
+    }
 
-        if (password.equals(confirmPassword)) {
-            if (password.length() <= 20) {
-                if (login.matches("^[a-zA-Z0-9_]*$")) {
-                    System.out.println("Пароль удовлетворяет заданным требованиям");
-                } else {
-                    throw new WrongPasswordException();
-                }
-            }
+
+    public static void checkPassword(String password, String confirmPassword) {
+
+        if (password.equals(confirmPassword) & password.length() <= 20 & password.matches("^[a-zA-Z0-9_]*$")) {
+            System.out.println("Пароль удовлетворяет заданным требованиям");
         } else {
             throw new WrongPasswordException();
         }
-
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ import exceptions.exceptions.WrongPasswordException;
 public class Main {
 
     public static void main(String[] args) {
-        String login = "Alex_1971";
+        String login = "Alex_197";
         String password = "AAA_1287";
         String confirmPassword = "AAA_1287";
 
-        Autorization check = new Autorization(login, password, confirmPassword);
+        Autorization check = new Autorization();
 
         try {
-            Autorization.check(login, password, confirmPassword);
+            Autorization.checkLogin(login);
+            Autorization.checkPassword(password, confirmPassword);
         } catch (WrongLoginException e) {
             System.out.println("Логин не удовлетворяет требованиям, попробуйте другой");
         } catch (WrongPasswordException e) {
             System.out.println("Пароль не удовлетворяет требованиям, попробуйте другой");
+        } finally {
+            System.out.println("Валидация окончена");
         }
 
     }
